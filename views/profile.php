9bcb7760c268315457600b05633cfb136a63f5bf
Marco van de Lindt <marcovandelindt@live.nl>
2017-02-03 10:18:52 +0100

    Added the profile view

---
+++ views/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Marco van de Lindt">
+		<meta name="description" content="Gastenboek">
+		<title><?php echo $title; ?></title>
+
+		<?php require 'requirements/header.html'; ?>
+	</head>
+	<body>		
+
+		<?php require 'requirements/footer.html'; ?>
+	</body>
+</html>
