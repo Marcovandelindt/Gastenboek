7316a48903d7e4419d548d9b653c4b8b76795e48
Marco van de Lindt <marcovandelindt@live.nl>
2017-02-03 14:34:53 +0100

    Added some standard text to the notloggedin error

--- errors/notloggedin.php
+++ errors/notloggedin.php
@@ -1,9 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-
-	</head>
-	<body>
-
-	</body>
-</html>
+<h1>Whoops, looks like you're not logged in yet!</h1>
+<h3>In order to view your profile, you must ne logged in, which you dan do here: <a href="../login">login!</a></h3>
