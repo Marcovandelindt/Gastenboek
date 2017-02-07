6f5dd940c44f2af1166d7c655a82020cc1456bad
Marco van de Lindt <marcovandelindt@live.nl>
2017-02-07 16:52:32 +0100

    Added the 404 file

---
+++ errors/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<mate name="author" content="Marco van de Lindt">
+		<meta name="description" content="Gastenboek - 404 Page">
+		<title>Whoops, this page could not be found...</title>
+
+		<link rel="stylesheet" type="text/css" href="../assets/css/style.css" media="screen">
+
+		<!-- Bootstrap CSS CDN -->
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body background="../assets/images/404.jpg">
+	</body>
+</html>
